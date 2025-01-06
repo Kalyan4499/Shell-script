50a0d5300297c449b3261742a396520d4d200c58
Kalyan Kumar Voleti <kalyankumar.voleti@gmail.com>
2025-01-07 00:32:18 +0530

    The script first verifies root access; it runs only if root access is available. Then, it checks if MySQL and Git are installed using exit codes. If not, it installs them

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -4,24 +4,64 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "Error: root privileges are required to run this command"
+    echo "ERROR: root privileges are required to run this command"
     exit 1 #other than 0
 fi
 
-dnf install mysql -y
+dnf list installed MySQL
 if [ $? -ne 0 ]
 then
-    echo "Failure: mysql is not installed"
-    exit 1
+    dnf install MySQL -y
+    if [ $? -ne 0 ]
+    then
+        echo "Failure: MySQL is not installed"
+        exit 1
+    else
+        echo "Success: MySQL is installed"
+    fi 
 else
-    echo "Success: mysql is installed"
-fi 
-
-dnf install git -y
-if [ $?-ne 0 ]
-then
-    echo "Failure: git is not installed"
-    exit 1
-else
-    echo "Success: git is installed"
+    echo "MySQL is already installed"
 fi
+    
+
+
+# dnf install MySQL -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo "Failure: MySQL is not installed"
+#         exit 1
+#     else
+#         echo "Success: MySQL is installed"
+#     fi
+
+
+
+
+
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Failure: git is not installed"
+        exit 1
+    else
+        echo "Success: git is installed"
+    fi 
+else
+    echo "git is already installed"
+fi
+
+
+
+
+# dnf install git -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo "Failure: git is not installed"
+#         exit 1
+#     else
+#         echo "Success: git is installed"
+#     fi 
