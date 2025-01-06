47dc94be5bcb1a9f58564c8a389c9926d72d6dc5
Kalyan Kumar Voleti <kalyankumar.voleti@gmail.com>
2025-01-06 15:19:19 +0530

    user input will hide

--- 05-variable user input .sh
+++ 05-variable user input .sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Please enter your username::" # text entered here will be added as value to variable
 
-read -s USERNAME  # Here USERNAME is the variable name
+read USERNAME  # Here USERNAME is the variable name
 
 echo "username entered: $USERNAME"
 
