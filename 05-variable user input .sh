e96949d3e0f58e1687f88c760955ec8d809b4f58
Kalyan Kumar Voleti <kalyankumar.voleti@gmail.com>
2025-01-06 15:12:08 +0530

    user input

--- 05-variable user input .sh
+++ 05-variable user input .sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Please enter your username::" # text entered here will be added as value to variable
 
-read USERNAME  # Here USERNAME is the variable name
+read -s USERNAME  # Here USERNAME is the variable name
 
 echo "username entered: $USERNAME"
