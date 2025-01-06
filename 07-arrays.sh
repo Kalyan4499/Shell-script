29deb6ad189dff17362be91385e3fe01933730fb
Kalyan Kumar Voleti <kalyankumar.voleti@gmail.com>
2025-01-06 15:59:37 +0530

    array

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,8 +2,8 @@
 
 MOVIES=("Extraction" "The Meg" "Spider-Man")
 
-echo "First movie is: {$MOVIES[0]}"
-echo "Second movie is: {$MOVIES[1]}"
-echo "Third movie is: {$MOVIES[2]}"
+echo "First movie is: ${MOVIES[0]}"
+echo "Second movie is: ${MOVIES[1]}"
+echo "Third movie is: ${MOVIES[2]}"
 
-echo "All movies are: {$MOVIES[@]}"
+echo "All movies are: ${MOVIES[@]}"
